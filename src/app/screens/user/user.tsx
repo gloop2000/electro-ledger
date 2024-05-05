c7abb292ac20ddf0105efe765490e3e4c78d30e6
Gloop 2000 <43214209+gloop2000@users.noreply.github.com>
2024-05-05 21:51:49 +0530

    setup: Add ipc

--- src/app/screens/user/user.tsx
+++ src/app/screens/user/user.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const User: React.FC = () => {
+  const ping = () => {
+    api.ping();
+  };
+
   console.log("User loaded");
   return (
     <div>
       <h1>User screen</h1>
+      <button onClick={ping}>Ping</button>
       <Link to="/">Go to Home Page</Link>
     </div>
   );
